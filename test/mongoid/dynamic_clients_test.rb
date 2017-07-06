f4aadb93f749417ceb080f76a221a33115e91527
pavlo <pavlo@cleverua.com>
2017-07-06 20:47:10 +0300

    Initial round of work on tests

--- test/mongoid/dynamic_clients_test.rb
+++ test/mongoid/dynamic_clients_test.rb
@@ -1,11 +1,41 @@
 require "test_helper"
 
 class Mongoid::DynamicClientsTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::Mongoid::DynamicClients::VERSION
   end
 
   def test_it_does_something_useful
-    assert true
+    assert Record.create(data: "data", client_identifier: "default")
   end
+
+end
+
+CLIENT_IDENTIFIERS = {
+    client1: {
+        database: "client1",
+        hosts: ['localhost:27017'],
+        options: {
+            user: 'client1_user',
+            password: 'client1_pass'
+        }
+    },
+    client2: {
+        database: "client2",
+        hosts: ['localhost:27017'],
+        options: {
+            user: 'client2_pass',
+            password: 'client2_pass'
+        }
+    }
+}
+
+class Record
+
+  include Mongoid::Document
+
+  field :data, type: String
+  field :client_identifier, type: String
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,10 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "mongoid/dynamic_clients"
 
+require 'active_support'
+require 'mongoid'
+
 require "minitest/autorun"
+
+ENV["MONGOID_ENV"] = "test"
+Mongoid.load!( File.join(File.dirname(__FILE__), "mongoid.yml") )
